1bdfe836818b9aef59b18fd270fa1ebf099a525a
Kevin Weber <kevin.weber@site-kitchen.com>
2012-11-16 22:37:48 -0500

    Added copyright and license notice to each file.

--- src/kevintweber/PhpunitW3CValidators/Assert/HTML.php
+++ src/kevintweber/PhpunitW3CValidators/Assert/HTML.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PhpunitW3CValidators package.
+ *
+ * (c) Kevin Weber <kevintweber@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace kevintweber\PhpunitW3CValidators\Assert;
 
 use kevintweber\PhpunitW3CValidators\Connector\HTMLConnector;
